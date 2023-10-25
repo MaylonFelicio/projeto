b8105ed13bda683515e7f24bd32a287169f233fa
Maylon Felicio <Maylon11felicio@gmail.com>
2023-10-25 09:04:43 -0300

    test

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function toggleMode() {
   //substituir a imagem 
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light 
-    img.setAttribute("src", "./assets/assets/avatar-light1.png")
+    img.setAttribute("src", "./assets/assets/avatar1.png");
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/assets/avatar1.png")
+    img.setAttribute("src", "./assets/assets/avatar-light1.png");
   }
 }
